4e34bbb15602e757273f4b4b405a838ea5b1707a
m4oyu <yuki.yamamoto0915@gmail.com>
2023-04-14 09:38:58 +0900

    commit

--- main.go
+++ main.go
@@ -57,6 +57,19 @@ func readInt() int {
 	return ret
 }
 
+func readInt2() (int, int) {
+	ret := strings.Split(readLine(), " ")
+	a, err := strconv.Atoi(ret[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+	b, err := strconv.Atoi(ret[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	return a, b
+}
+
 func readIntArray() []int {
 	ret := make([]int, 0)
 	tmp := strings.Split(readLine(), " ")
@@ -87,29 +100,101 @@ func min(a, b int) int {
 }
 
 func main() {
-	n := readInt()
-	s := readLine()
+	var h, w int
+	fmt.Scan(&h)
+	fmt.Scan(&w)
+	q := readInt()
 
-	atcoder := "atcoder "
-
-	dp := make([][]int, n+1)
-	for i := 0; i < n+1; i++ {
-		dp[i] = []int{0, 0, 0, 0, 0, 0, 0, 0}
+	color := make([]int, h*w)
+	for i := 0; i < h*w; i++ {
+		color[i] = 0
 	}
 
-	dp[0][0] = 1
-	for i := 0; i < n; i++ {
-		for j := 0; j < 8; j++ {
-			dp[i+1][j] += dp[i][j]
-			if s[i] == atcoder[j] {
-				dp[i+1][j+1] += dp[i][j]
+	uf := NewUnionFind(h * w)
+
+	diff := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+
+	for i := 0; i < q; i++ {
+		// query := readIntArray()
+		// fmt.Println(query)
+
+		var query int
+		fmt.Scan(&query)
+
+		if query == 1 {
+			var r, c int
+			fmt.Scanf("%d", &r)
+			fmt.Scanf("%d", &c)
+
+			target := w*(r-1) + c - 1
+			color[target] = 1
+
+			for _, v := range diff {
+				dr := r + v[0]
+				dc := c + v[1]
+				if dr <= 0 || h < dr || dc <= 0 || w < dc {
+					continue
+				}
+				if color[w*(dr-1)+dc-1] == 1 {
+					uf.merge(target, w*(dr-1)+dc-1)
+				}
 			}
 		}
-		for j := 0; j < 8; j++ {
-			dp[i+1][j] %= mod
+
+		if query == 2 {
+			var ra, ca, rb, cb int
+			fmt.Scanf("%d", &ra)
+			fmt.Scanf("%d", &ca)
+			fmt.Scanf("%d", &rb)
+			fmt.Scanf("%d", &cb)
+
+			if uf.same(w*(ra-1)+ca-1, w*(rb-1)+cb-1) && color[w*(ra-1)+ca-1] == 1 && color[w*(rb-1)+cb-1] == 1 {
+				fmt.Println("Yes")
+			} else {
+				fmt.Println("No")
+			}
 		}
+
 	}
 
-	fmt.Println(dp[len(s)][7])
+}
 
+func NewUnionFind(n int) *Union_Find {
+	tree := make([]int, n)
+	for i := 0; i < n; i++ {
+		tree[i] = -1
+	}
+	return &Union_Find{
+		parentsOrSize: tree,
+	}
+}
+
+type Union_Find struct {
+	parentsOrSize []int
+}
+
+func (uf *Union_Find) find(x int) int {
+	if uf.parentsOrSize[x] < 0 {
+		return x
+	}
+	uf.parentsOrSize[x] = uf.find(uf.parentsOrSize[x])
+	return uf.parentsOrSize[x]
+}
+
+func (uf *Union_Find) merge(a, b int) {
+	a = uf.find(a)
+	b = uf.find(b)
+	if a != b {
+		if -uf.parentsOrSize[a] < -uf.parentsOrSize[b] {
+			tmp := a
+			a = b
+			b = tmp
+		}
+		uf.parentsOrSize[a] += uf.parentsOrSize[b]
+		uf.parentsOrSize[b] = a
+	}
+}
+
+func (uf *Union_Find) same(a, b int) bool {
+	return uf.find(a) == uf.find(b)
 }
