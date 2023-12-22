d68f3e2614ccf667eaa0d31bdf4d711a7f04f28d
m4oyu <yuki.yamamoto0915@gmail.com>
2023-12-22 17:51:13 +0900

    B wa

---
+++ ABC/333/a/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "debug" {
+			debug()
+		}
+	}
+	const buf = 200100
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, buf), buf)
+}
+
+func debug() {
+	input, err := os.Open("./input")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no input file")
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(input)
+}
+
+func main() {
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	for i := 0; i < n; i++ {
+		fmt.Print(n)
+	}
+	fmt.Println()
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "debug" {
+			debug()
+		}
+	}
+	const buf = 200100
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, buf), buf)
+}
+
+func debug() {
+	input, err := os.Open("./input")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no input file")
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(input)
+}
+
+func main() {
+	sc.Scan()
+	s := sc.Text()
+	sc.Scan()
+	t := sc.Text()
+
+	diffs := math.Abs(float64(int(s[0]) - int(s[1])))
+	difft := math.Abs(float64(int(t[0]) - int(t[1])))
+
+	if ((diffs == 1 || diffs == 4) && (difft == 1 || difft == 4)) || (diffs == 2 && difft == 2) {
+		fmt.Println("Yes")
+		return
+	}
+	fmt.Println("No")
+}
