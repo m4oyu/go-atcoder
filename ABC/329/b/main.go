7695c80fe47731a6e9f4f8a1a8fac233868ce1f7
m4oyu <yuki.yamamoto0915@gmail.com>
2024-01-10 12:46:28 +0900

    solve conflict

--- ABC/329/b/main.go
+++ ABC/329/b/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -12,7 +13,12 @@ var sc = bufio.NewScanner(os.Stdin)
 func init() {
 	if len(os.Args) >= 2 {
 		if os.Args[1] == "debug" {
-			debug()
+			input, err := os.Open("./input")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			sc = bufio.NewScanner(input)
 		}
 	}
 	const buf = 200100
@@ -20,33 +26,42 @@ func init() {
 	sc.Buffer(make([]byte, buf), buf)
 }
 
-func debug() {
-	input, err := os.Open("./input")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "There is no input file")
-		os.Exit(1)
-	}
-	sc = bufio.NewScanner(input)
-}
-
 func main() {
-	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
+	var repunit []int
+	repunit = append(repunit, 1)
+	a := "1"
+	for i := 0; i < 11; i++ {
+		a = a + "1"
+		b, _ := strconv.Atoi(a)
+		repunit = append(repunit, b)
+	}
 
-	max := 0
-	ans := 0
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		a, _ := strconv.Atoi(sc.Text())
-		if max < a {
-			ans = max
-			max = a
-		} else if max == a {
-			continue
-		} else if ans < a {
-			ans = a
+	var list []int
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 12; j++ {
+			for k := 0; k < 12; k++ {
+				sum := repunit[i] + repunit[j] + repunit[k]
+				if !contains(list, sum) {
+					list = append(list, sum)
+				}
+			}
 		}
 	}
 
-	fmt.Println(ans)
+	sort.Slice(list, func(i, j int) bool {
+		return list[i] < list[j]
+	})
+
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	fmt.Println(list[n-1])
+
+}
+func contains(arr []int, val int) bool {
+	for _, item := range arr {
+		if item == val {
+			return true
+		}
+	}
+	return false
 }
